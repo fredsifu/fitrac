db3cfe6913ec5b9f31a3024dc18f088f65fcf526
fredsifu <fredsifu@hotmail.com>
2016-03-16 04:17:30 +0000

    Added friendship feature

--- config/routes.rb
+++ config/routes.rb
@@ -7,9 +7,14 @@ Rails.application.routes.draw do
   root 'welcome#index'
   
   get 'my_portfolio', to: 'users#my_portfolio'
+  get 'my_friends', to: 'users#my_friends'
   get 'search_stocks', to: 'stocks#search'
+  get 'search_friends', to: 'users#search'
+  post 'add_friend', to: 'users#add_friend'
   
   resources :user_stocks, except: [:show, :edit, :update]
+  resources :users, only: [:show]
+  resources :friendships
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
